82ac89df9dc0497ffa18c60bf3a4c7bac12ec917
Bryan Rosander <bryanrosander@gmail.com>
2016-07-05 13:17:34 -0400

    incorporating port id replacement with run.sh

--- run.sh
+++ run.sh
@@ -11,7 +11,17 @@ if [ -n "$1" ]; then
 
   chmod +x "$CONFIG_SCRIPT"
 
-  "$CONFIG_SCRIPT" transform "$1" "$CONF_DIR/config.yml"
+  if [ "InvokeHttpMiNiFiTemplateTest.xml" = "$( basename "$1" )" ]; then
+    if [ -z "$2" ]; then
+      echo "Must specify input port id as arg 2 for InvokeHttpMiNiFiTemplateTest.xml"
+      exit 1
+    fi
+    
+    sed "s/b23a4621-cf19-42e6-967c-ffd3716e6a24/$2/g" "$DIR/templates/InvokeHttpMiNiFiTemplateTest.xml" > /tmp/InvokeHttpMiNiFiTemplateTest.xml
+    "$CONFIG_SCRIPT" transform "/tmp/InvokeHttpMiNiFiTemplateTest.xml" "$CONF_DIR/config.yml"
+  else
+    "$CONFIG_SCRIPT" transform "$1" "$CONF_DIR/config.yml"
+  fi
 
   if [ $? -ne 0 ]; then
     exit 1
