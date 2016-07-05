2f6d0d90b9a29caf6798144f424f01917721297e
Bryan Rosander <bryanrosander@gmail.com>
2016-07-04 23:33:21 -0400

    run without args uses existing config.yml, use minifi network

--- run.sh
+++ run.sh
@@ -6,14 +6,16 @@ TOOLKIT_DIR="$DIR/toolkit"
 ARCHIVE_DIR="$DIR/archive"
 CONF_DIR="$DIR/conf"
 
-CONFIG_SCRIPT="$(find $TOOLKIT_DIR -name config.sh | head -n 1)"
+if [ -n "$1" ]; then
+  CONFIG_SCRIPT="$(find $TOOLKIT_DIR -name config.sh | head -n 1)"
 
-chmod +x "$CONFIG_SCRIPT"
+  chmod +x "$CONFIG_SCRIPT"
 
-"$CONFIG_SCRIPT" transform "$1" "$CONF_DIR/config.yml"
+  "$CONFIG_SCRIPT" transform "$1" "$CONF_DIR/config.yml"
 
-if [ $? -ne 0 ]; then
-  exit 1
+  if [ $? -ne 0 ]; then
+    exit 1
+  fi
 fi
 
-docker run -ti -v "$ARCHIVE_DIR":/opt/minifi-archive -v "$CONF_DIR":/opt/minifi-conf -p 8081:8081 --rm minifi
+docker run -ti -v "$ARCHIVE_DIR":/opt/minifi-archive -v "$CONF_DIR":/opt/minifi-conf -p 8081:8081 --rm --net minifi --hostname minifi --name minifi minifi
